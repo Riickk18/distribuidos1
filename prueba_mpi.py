e35a60d0f7255de25f8fc19e6d70fdc07c4f3a42
albertogarciacerruto <albertogarciacerruto@gmail.com>
2018-11-04 21:12:31 -0400

    Arreglado .py

--- prueba_mpi.py
+++ prueba_mpi.py
@@ -27,32 +27,26 @@ name = MPI.Get_processor_name()
 if rank == 0:
    sys.stdout.write('Soy nodo maestro %d \n' % (rank))
 if rank == 1:
-   print('Soy el Proceso 1')
+   print('Soy el Proceso %d \n' % (rank))
    llenar_logs()
 if rank == 2:
-   print('Soy el Proceso 2')
+   print('Soy el Proceso %d \n' % (rank))
    llenar_logs()
 if rank == 3:
-   print('Soy el Proceso 3')
+   print('Soy el Proceso %d \n' % (rank))
    llenar_logs()
 if rank == 4:
-   print('Soy el Proceso 4')
+   print('Soy el Proceso %d \n' % (rank))
    llenar_logs()
 if rank == 5:
-   print('Soy el Proceso 5')
-   llenar_logs()
-if rank == 5:
-   print('Soy el Proceso 5')
+   print('Soy el Proceso %d \n' % (rank))
    llenar_logs()
 if rank == 6:
-   print('Soy el Proceso 6')
+   print('Soy el Proceso %d \n' % (rank))
    llenar_logs()
 if rank == 7:
-   print('Soy el Proceso 7')
+   print('Soy el Proceso %d \n' % (rank))
    llenar_logs()
 if rank == 8:
-   print('Soy el Proceso 8')
-   llenar_logs()
-if rank == 9:
-   print('Soy el Proceso 9')
+   print('Soy el Proceso %d \n' % (rank))
    llenar_logs()
