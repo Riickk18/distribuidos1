9f95d96f5541460fb0f10966124e06477fbcd81f
Unknown <richard_orlan@hotmail.com>
2018-11-06 15:03:18 -0400

    Implementacion de Send y Recv
    Se implementa el send y recv para notificarle a los nodos que ya se procesaron los logs

--- prueba_mpi.py
+++ prueba_mpi.py
@@ -9,44 +9,24 @@ import gzip
 
 logs = []
 nombreArchivos = ['/home/public/201915/muestra1/audit.log.2018-10-03.gz']
-
-def llenar_logs():
-       f = ''
-       global logs
-       for x in nombreArchivos:
-           f=gzip.GzipFile(fileobj=open(x, 'rb'))
-           cadena = f.read()
-           logs += cadena.split('\n')
-           f.close()
-       print(logs[0:3])
-
-size = MPI.COMM_WORLD.Get_size()
-rank = MPI.COMM_WORLD.Get_rank()
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
 name = MPI.Get_processor_name()
 
-if rank == 0:
-   sys.stdout.write('Soy nodo maestro %d \n' % (rank))
-if rank == 1:
-   print('Soy el Proceso %d \n' % (rank))
-   llenar_logs()
-if rank == 2:
-   print('Soy el Proceso %d \n' % (rank))
-   llenar_logs()
-if rank == 3:
-   print('Soy el Proceso %d \n' % (rank))
-   llenar_logs()
-if rank == 4:
-   print('Soy el Proceso %d \n' % (rank))
-   llenar_logs()
-if rank == 5:
-   print('Soy el Proceso %d \n' % (rank))
-   llenar_logs()
-if rank == 6:
-   print('Soy el Proceso %d \n' % (rank))
-   llenar_logs()
-if rank == 7:
-   print('Soy el Proceso %d \n' % (rank))
-   llenar_logs()
-if rank == 8:
-   print('Soy el Proceso %d \n' % (rank))
-   llenar_logs()
+def llenar_logs():
+    if rank == 0:
+        f = ''
+        global logs
+        for x in nombreArchivos:
+            f=gzip.GzipFile(fileobj=open(x, 'rb'))
+            cadena = f.read()
+            logs += cadena.split('\n')
+            f.close()
+        comm.bcast('completeLogs', 0)
+    else:
+        data = comm.recv(source=0)
+        print('Soy ',rank,' y recibi ',data)
+ 
+if __name__ == '__main__':
+    llenar_logs()
