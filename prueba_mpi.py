d78610f78145f7099b4eb363385e2dd78f7c54a0
albertogarciacerruto <albertogarciacerruto@gmail.com>
2018-11-06 18:38:04 -0400

    Aplicando Send y Recive

--- prueba_mpi.py
+++ prueba_mpi.py
@@ -15,7 +15,6 @@ rank = comm.Get_rank()
 name = MPI.Get_processor_name()
 
 def llenar_logs():
-    if rank == 0:
         f = ''
         global logs
         for x in nombreArchivos:
@@ -23,10 +22,40 @@ def llenar_logs():
             cadena = f.read()
             logs += cadena.split('\n')
             f.close()
-        comm.bcast('completeLogs', 0)
-    else:
-        data = comm.recv(source=0)
-        print('Soy ',rank,' y recibi ',data)
- 
-if __name__ == '__main__':
+
+if rank == 0:
     llenar_logs()
+    data = 'Archivos de Logs Procesados'
+    comm.send(data, dest=1)
+    comm.send(data, dest=2)
+    comm.send(data, dest=3)
+    comm.send(data, dest=4)
+    comm.send(data, dest=5)
+    comm.send(data, dest=6)
+    comm.send(data, dest=7)
+    comm.send(data, dest=8)
+print 'Del Rank',name,'Enviamos',data
+if rank == 1:
+    data = comm.recv(source=0)
+    print 'En Nodo',name, 'Recibimos:',data
+if rank == 2:
+    data = comm.recv(source=0)
+    print 'En Nodo',name, 'Recibimos:',data
+if rank == 3:
+    data = comm.recv(source=0)
+    print 'En Nodo',name, 'Recibimos:',data
+if rank == 4:
+    data = comm.recv(source=0)
+    print 'En Nodo',name, 'Recibimos:',data
+if rank == 5:
+    data = comm.recv(source=0)
+    print 'En Nodo',name, 'Recibimos:',data
+if rank == 6:
+    data = comm.recv(source=0)
+    print 'En Nodo',name, 'Recibimos:',data
+if rank == 7:
+    data = comm.recv(source=0)
+    print 'En Nodo',name, 'Recibimos:',data
+if rank == 8:
+    data = comm.recv(source=0)
+    print 'En Nodo',name, 'Recibimos:',data
