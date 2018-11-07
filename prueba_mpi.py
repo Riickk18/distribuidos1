97c9bb8ed7441898ebd19775352b8e569d50f03b
albertogarciacerruto <albertogarciacerruto@gmail.com>
2018-11-06 20:20:42 -0400

    Enviando linea de logs a los nodos con 8 Procesadores activos + maestro

--- prueba_mpi.py
+++ prueba_mpi.py
@@ -25,37 +25,37 @@ def llenar_logs():
 
 if rank == 0:
     llenar_logs()
-    data = 'Archivos de Logs Procesados'
-    comm.send(data, dest=1)
-    comm.send(data, dest=2)
-    comm.send(data, dest=3)
-    comm.send(data, dest=4)
-    comm.send(data, dest=5)
-    comm.send(data, dest=6)
-    comm.send(data, dest=7)
-    comm.send(data, dest=8)
-print 'Del Rank',name,'Enviamos',data
+    data = logs
+    comm.send(data[0:1], dest=1, tag=11)
+    comm.send(data[1:2], dest=2, tag=12)
+    comm.send(data[2:3], dest=3, tag=13)
+    comm.send(data[3:4], dest=4, tag=14)
+    comm.send(data[4:5], dest=5, tag=15)
+    comm.send(data[5:6], dest=6, tag=16)
+    comm.send(data[6:7], dest=7, tag=17)
+    comm.send(data[7:8], dest=8, tag=18)
+    print 'Del Rank',name,'Enviamos las lineas de logs a procesar'
 if rank == 1:
-    data = comm.recv(source=0)
-    print 'En Nodo',name, 'Recibimos:',data
+    data = comm.recv(source=0, tag=11)
+    print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:',data
 if rank == 2:
-    data = comm.recv(source=0)
-    print 'En Nodo',name, 'Recibimos:',data
+    data = comm.recv(source=0, tag=12)
+    print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:',data
 if rank == 3:
-    data = comm.recv(source=0)
-    print 'En Nodo',name, 'Recibimos:',data
+    data = comm.recv(source=0, tag=13)
+    print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:',data
 if rank == 4:
-    data = comm.recv(source=0)
-    print 'En Nodo',name, 'Recibimos:',data
+    data = comm.recv(source=0, tag=14)
+    print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:',data
 if rank == 5:
-    data = comm.recv(source=0)
-    print 'En Nodo',name, 'Recibimos:',data
+    data = comm.recv(source=0, tag=15)
+    print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:',data
 if rank == 6:
-    data = comm.recv(source=0)
-    print 'En Nodo',name, 'Recibimos:',data
+    data = comm.recv(source=0, tag=16)
+    print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:',data
 if rank == 7:
-    data = comm.recv(source=0)
-    print 'En Nodo',name, 'Recibimos:',data
+    data = comm.recv(source=0, tag=17)
+    print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:',data
 if rank == 8:
-    data = comm.recv(source=0)
-    print 'En Nodo',name, 'Recibimos:',data
+    data = comm.recv(source=0, tag=18)
+    print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:',data
