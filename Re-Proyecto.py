87672328882a09c11108e0f5b6b36f44ed0332dd
albertogarciacerruto <albertogarciacerruto@gmail.com>
2018-11-18 18:50:03 -0400

    Re-Proyecto

---
+++ Re-Proyecto.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+from mpi4py import MPI
+#import geoip2.database
+#import pygeoip
+
+#Inicializar el COMM_WORLD y sus mecanismos
+comm = MPI.COMM_WORLD
+rank = comm.rank
+size = comm.Get_size()
+
+#Arreglo Principal
+arregloPrincipal = [] #Vectormaster
+
+#Arreglos y Contadores de algunos Arreglos
+arregloIp = []
+contadorIp = []
+arregloIpLocal = []
+contadorIpLocal = []
+arregloPais = []
+contadorPais = []
+arregloCiudad = []
+contadorCiudad = []
+arregloHora = []
+contadorHora = []
+arregloCorreo = []
+contadorCorreo = []
+arregloProtocolo = []
+contadorProtocolo = []
+
+#Fragmento
+fragmento = 0
+
+#Verifica cuantos procesos son y crear Arreglos necesarios
+def picar(size):
+    global fragmento
+    x = 0
+    while(x<size):
+        arregloPrincipal.append([])
+        x += 1
+    fragmento = size-1
+
+#Leemos los Logs e ingresa en el arreglo
+def leerLog(archivo, longitud):
+    global fragmento
+    x = 0
+    y = 0
+    while(x<longitud):
+        if(y>fragmento):
+            y = 0
+        txt = archivo.readline()
+        arregloPrincipal[y].append(txt)
+        y += 1
+        x += 1
+
+#Organizamos las lineas
+def organizar(arreglo, principal, contador):
+    longitud = len(arreglo)
+    x = 0
+    while(x<longitud):
+        arreglo1 = arreglo[x]
+        arreglo2 = len(arreglo1)
+        y = 0
+        while(y<arreglo2):
+            w = arreglo1[y]
+            if w in principal:
+                index = principal.index(w)
+                contador[index] += 1
+            else:
+                principal.append(w)
+                contador.append(1)
+            y += 1
+        x += 1 #Una menos didentacion
+
+#Organizamos de forma descendente
+def top20(principal, contador):
+    for i in range(len(contador)-1,0,-1):
+        for j in range(i):
+            if contador[j]>contador[j+1]:
+                temporal=contador[j]
+                temporal1=principal[j]
+                contador[j]=contador[j+1]
+                principal[j]=principal[j+1]
+                contador[j+1]=temporal
+                principal[j+1]=temporal1
+    i = 0
+    j = len(contador)-1
+    temporal = []
+    temporal1 = []
+    while(i<len(contador)):
+        temporal.append(contador[j])
+        temporal1.append(principal[j])
+        i += 1
+        j -= 1
+    i = 0
+    while(i<len(contador)):
+        principal[i] = temporal1[i]
+        contador[i] = temporal[i]
+        i += 1
+
+#Mostramos
+def resultado(principal, contador):
+    x = 0
+    y = 0
+    sum = 0
+    longitud = len(contador)
+    if(longitud>20):
+        longitud = 20
+    while(y<len(contador)):
+        sum = sum+contador[y]
+        y += 1
+    while(x<longitud):
+        p = round((contador[x]/float(sum))*100,2)
+        print(str(x+1) + ". - " + principal[x] + ". - Ataques: " + str(contador[x]) + ". - Porcentaje: " + str(p))
+        x += 1
+
+################################### M P I ########################################
+if(rank == 0):
+    picar(size)
+    archivo = open("audit.log.2018-10-04", "r")
+    longitud = len(open("audit.log.2018-10-04").readline())
+    leerLog(archivo, longitud)
+    archivo.close()
+else:
+    arregloPrincipal = None
+
+scatter = comm.scatter(arregloPrincipal, root=0)
+
+#Procesamiento
+longitud = len(scatter)
+x = 0
+listaIp = []
+listaIpLocal = []
+listaPais = []
+listaCiudad = []
+listaProtocolo = []
+listaHora = []
+listaCorreo = []
+while(x<longitud):
+    info = scatter[x]
+    confirma = info[24:28]
+    if(confirma == "WARN"):
+        #gi = pygeoip.GeoIP('GeoLiteCity.dat')
+        ip1 = info.split("oip=")
+        ip2 = ip1[1].split(";")
+        ip = ip2[0]
+        hora = info[11:13]
+        correo1 = info.split("account=")
+        correo2 = correo1[1].split(";")
+        correo = correo2[0]
+        protocolo1 = info.split("protocol=")
+        protocolo2 = protocolo1[1].split(";")
+        protocolo = protocolo2[0]
+        confirmaIpLocal = ip[0:7]
+        cofirmaIpNone = ip[0:6]
+        if(confirmaIpLocal == "192.168"):
+            listaIpLocal.append(ip)
+            listaCiudad.append("LocalHost")
+            listaPais.append("LocalHost")
+        else:
+            #pais = gi.country_name_by_addr(ip)
+            #ciudad = gi.time_zone_by_addr(ip)
+            #listaCiudad.append(ciudad)
+            #listaPais.append(pais)
+            listaIp.append(ip)
+            listaHora.append(hora)
+            listaCorreo.append(correo)
+            listaProtocolo.append(protocolo)
+    x += 1
+
+#Devolver Scatter con Gatter
+ips = comm.gather(listaIp, root=0)
+ipsLocal = comm.gather(listaIpLocal, root=0)
+#paises = comm.gather(listaPais, root=0)
+#ciudades = comm.gather(listaCiudad, root=0)
+horas = comm.gather(listaHora, root=0)
+correos = comm.gather(listaCorreo, root=0)
+protocolos = comm.gather(listaProtocolo, root=0)
+
+if(rank ==0):
+    organizar(ips, arregloIp, contadorIp)
+    organizar(ipsLocal, arregloIpLocal, contadorIpLocal)
+    #organizar(paises, arregloPais, contadorPais)
+    #organizar(ciudades, arregloCiudad, contadorCiudad)
+    organizar(horas, arregloHora, contadorHora)
+    organizar(correos, arregloCorreo, contadorCorreo)
+    organizar(protocolos, arregloProtocolo, contadorProtocolo)
+    top20(arregloIp, contadorIp)
+    top20(arregloIpLocal, contadorIpLocal)
+    #top20(arregloPais, contadorPais)
+    #top20(arregloCiudad, contadorCiudad)
+    top20(arregloHora, contadorHora)
+    top20(arregloCorreo, contadorCorreo)
+    top20(arregloProtocolo, contadorProtocolo)
+    print('----------------- 20 IP -------------')
+    resultado(arregloIp,contadorIp)
+    print('----------------- 20 IP-Local -------------')
+    resultado(arregloIpLocal,contadorIpLocal)
+    #print('----------------- 20 PAISES -------------')
+    #resultado(arregloPais,contadorPais)
+    #print('----------------- 20 CIUDADES -------------')
+    #resultado(arregloCiudad,contadorCiudad)
+    print('----------------- 20 HORAS -------------')
+    resultado(arregloHora,contadorHora)
+    print('----------------- 20 CORREOS -------------')
+    resultado(arregloCorreo,contadorCorreo)
+    print('----------------- 20 PROTOCOLOS -------------')
+    resultado(arregloProtocolo,contadorProtocolo)
--- prueba_mpi.py
+++ prueba_mpi.py
@@ -11,53 +11,108 @@ rank = comm.Get_rank()
 name = MPI.Get_processor_name()
 
 def llenar_logs():
-        f = ''
-        global logs
-        for x in nombreArchivos:
-            f=gzip.GzipFile(fileobj=open(x, 'rb'))
-            cadena = f.read()
-            logs += cadena.split('\n')
-            f.close()
+    f = ''
+    logs = []
+    contador = 1
+    for x in nombreArchivos:
+        f=gzip.GzipFile(fileobj=open(x, 'rb'))
+        cadena = f.read()
+        logs += cadena.split('\n')
+        print('Archivo ' + str(contador)+', cantidad de lineas: '+str(len(logs)))
+        f.close()
+    print(len(logs))
+    return logs
+
+def llenar_logs_Warn():
+    f = ''
+    arregloWarns = []
+    global logs
+    for x in nombreArchivos:
+        f=gzip.GzipFile(fileobj=open(x, 'rb'))
+        cadena = f.read()
+        logs += cadena.split('\n')
+        f.close()
+    for x in range(len(logs)-1):
+        if 'WARN' in logs[x]:
+            arregloWarns.append(logs[x])
+    return arregloWarns
+
 
 def sacar_top20(arreglo):
     diccionario = {}
     arregloTop20 = []
+    arregloClave=[]
+    arregloValor=[]
     for elemento in arreglo:
-        if elemento in diccionario:
+        if elemento in diccionario:#le sumo uno a un elemento existente
             diccionario[elemento] += 1
-        else:
+        else:#Creo el elemento en el diccionario
             diccionario[elemento] = 1
+    tuplas = diccionario.items()
+    for x in tuplas:
+        arregloClave.append(x[0])
+        arregloValor.append(x[1])
     for x in range(20):
-        claveMayor = max(diccionario.keys())
-        arregloTop20.append(diccionario[claveMayor])
-        del diccionario[claveMayor]
+        numeroMayor = arregloValor[0]
+        posicionMayor = 0
+        for y in range(1,len(arregloValor)-1):
+            if arregloValor[y] > numeroMayor:
+                numeroMayor = arregloValor[y]
+                posicionMayor = y
+        arregloTop20.append([arregloClave[posicionMayor], arregloValor[posicionMayor]])
+        arregloClave.pop(y)
+        arregloValor.pop(y)
+
+    # claveMayor = max(diccionario.keys())
+    # arregloTop20.append(diccionario[claveMayor])
+    # del diccionario[claveMayor]
     return arregloTop20
 
 def picar_ip(data):
-        arregloips = []
-        arregloemails = []
-        arregloTime = []
-        for x in data:
-            #extraer ip
-            inicioip = x.find('oip')+4
-            finip = x[inicioip:inicioip+20].find(';')+inicioip
-            if x[inicioip:finip] != '':
-                arregloips.append(x[inicioip:finip])
-            #extraer correo
-            inicioem = x.find('account')+8
-            finem = x[inicioem:].find(';')+inicioem
-            correo = x[inicioem:finem]
-            if correo != '' and re.match('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$',correo.lower()):
-                arregloemails.append(x[inicioem:finem])
-            #extraer horas
-            finTime = x.find(',')
-            inicioTime = finTime-8
-            if x[inicioTime:finTime] != '':
-                arregloTime.append(x[inicioTime:finTime])
-        return sacar_top20(arregloips),sacar_top20(arregloemails), sacar_top20(arregloTime)
+    arregloips = []
+    arregloemails = []
+    arregloTime = []
+    for x in data:
+        #extraer ip
+        inicioip = x.find('oip')+4
+        finip = x[inicioip:inicioip+20].find(';')+inicioip
+        if x[inicioip:finip] != '':
+            arregloips.append(x[inicioip:finip])
+        #extraer correo
+        inicioem = x.find('account')+8
+        finem = x[inicioem:].find(';')+inicioem
+        correo = x[inicioem:finem]
+        if correo != '' and re.match('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$',correo.lower()):
+            arregloemails.append(x[inicioem:finem])
+        #extraer horas
+        finTime = x.find(',')
+        inicioTime = finTime-8
+        if x[inicioTime:finTime] != '':
+            arregloTime.append(x[inicioTime:finTime])
+    return sacar_top20(arregloips),sacar_top20(arregloemails), sacar_top20(arregloTime)
+
+def partir_logs():
+    tamano = len(logs)/(size-1)
+    tamano = int(tamano)
+    arregloRetorno = []
+    for x in range(size-2):
+        if x == 0:
+            arregloRetorno.append([0,tamano])
+        else:
+            if x != size-2:
+                arregloRetorno.append([(tamano*x)+1,tamano*(x+1)])
+            else:
+                arregloRetorno.append([(tamano*x)+1, len(logs)])
+    return arregloRetorno
 
 if rank == 0:
-    llenar_logs()
+    print 'Del Rank',name,'Enviamos las lineas de logs a procesar'
+    logs = llenar_logs()
+    #indice = partir_logs()
+    #contador = 1
+    #while (contador < 8):
+        #comm.send(logs[indice[contador-1][0]:indice[contador-1][1]], dest=contador, tag=contador+10)
+        #contador += 1
     comm.send(logs[0:100], dest=1, tag=11)
     comm.send(logs[101:200], dest=2, tag=12)
     comm.send(logs[201:300], dest=3, tag=13)
@@ -65,7 +120,6 @@ if rank == 0:
     comm.send(logs[401:500], dest=5, tag=15)
     comm.send(logs[501:600], dest=6, tag=16)
     comm.send(logs[601:700], dest=7, tag=17)
-    print 'Del Rank',name,'Enviamos las lineas de logs a procesar'
 if rank == 1:
     data = comm.recv(source=0, tag=11)
     print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:'
@@ -75,29 +129,29 @@ if rank == 2:
     data = comm.recv(source=0, tag=12)
     print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:'
     retorno = picar_ip(data)
-    print retorno
+    #print retorno
 if rank == 3:
     data = comm.recv(source=0, tag=13)
     print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:'
     retorno = picar_ip(data)
-    print retorno
+    #print retorno
 if rank == 4:
     data = comm.recv(source=0, tag=14)
     print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:'
     retorno = picar_ip(data)
-    print retorno
+    #print retorno
 if rank == 5:
     data = comm.recv(source=0, tag=15)
     print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:'
     retorno = picar_ip(data)
-    print retorno
+    #print retorno
 if rank == 6:
     data = comm.recv(source=0, tag=16)
     print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:'
     retorno = picar_ip(data)
-    print retorno
+    #print retorno
 if rank == 7:
     data = comm.recv(source=0, tag=17)
     print 'En Nodo de Nombre ',name, 'Con Rango ', rank, 'Recibimos:'
     retorno = picar_ip(data)
-    print retorno
+    #print retorno
